4dd21be6b2231e05a2542368718ddc9e692ec182
WlanKabl <55993320+PhilippStecher@users.noreply.github.com>
2021-04-29 16:17:57 +0200

    AI can learn on its own

--- RockPaperScissor/index.js
+++ RockPaperScissor/index.js
@@ -7,6 +7,9 @@ const GamePath = __dirname + '/results/games.json';
 const PossibleMoves = ["rock", "paper", "scissor"];
 const PossibleInputs = PossibleMoves;
 
+const RulesPath = __dirname + "/Rules.json";
+var AutoLearn = true;
+
 async function askQuestion(query) {
     const rl = await readline.createInterface({
         input: process.stdin,
@@ -42,15 +45,24 @@ SaveGame = (CurrentPlayerMove, AIMove, GameRes) => {
 
 StartGame = (CurrentPlayerMove) => {
     AImove.RockPaperScissor(CurrentPlayerMove).then(AIMove => {
-        console.log("The AI choose: " + AIMove);
-        askQuestion("Did i beat you? ").then(YourAwnser => {
-            var YourAwnserS = YourAwnser.toString().toLowerCase();
-            if (YourAwnserS == "1" || YourAwnserS == "true" || YourAwnserS == "yes" || YourAwnserS == "y") {
+        console.log("The AI choose: '" + AIMove + "' against '" + CurrentPlayerMove + "'");
+        if (AutoLearn) {
+            var RulesJson = JSON.parse(fs.readFileSync(RulesPath, "utf-8"));
+            if (AIMove == RulesJson[0][CurrentPlayerMove]) {
                 SaveGame(CurrentPlayerMove, AIMove, "1");
             } else {
                 SaveGame(CurrentPlayerMove, AIMove, "0");
             }
-        })  
+        } else {
+            askQuestion("Did i beat you? ").then(YourAwnser => {
+                var YourAwnserS = YourAwnser.toString().toLowerCase();
+                if (YourAwnserS == "1" || YourAwnserS == "true" || YourAwnserS == "yes" || YourAwnserS == "y") {
+                    SaveGame(CurrentPlayerMove, AIMove, "1");
+                } else {
+                    SaveGame(CurrentPlayerMove, AIMove, "0");
+                }
+            }) 
+        }
     })
       
 }
@@ -59,6 +71,13 @@ StartUp = () => {
     var YourChoice = process.argv.slice(2).toString().toLowerCase();
     if (YourChoice == undefined) {
         console.log("ERROR: Please enter a move as start parameter")
+        return;
+    }
+    if (YourChoice == "auto") {
+        AutoLearn = true;
+        for (var x = 0; x < 1000; x++) {
+            StartGame(PossibleMoves[RandomNum(0, PossibleMoves.length)])
+        }
         return;
     }
 
@@ -72,7 +91,6 @@ StartUp = () => {
             }
         }
     })
-
     if (WordFound) {
         console.log("Starting game with: " + CurrentPlayerMove)
         StartGame(CurrentPlayerMove);
