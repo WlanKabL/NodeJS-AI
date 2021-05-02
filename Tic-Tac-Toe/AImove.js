637f71e02f6e6b6251d57e82efe7fce9ec10d3e7
WlanKabl <55993320+PhilippStecher@users.noreply.github.com>
2021-05-02 23:17:46 +0200

    unbeatable AI

--- Tic-Tac-Toe/AImove.js
+++ Tic-Tac-Toe/AImove.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const { exit } = require('process');
 const OldGamesPath = __dirname + "/results/games.json";
-
+const RulesPath = __dirname + "/Rules.json";
 
 RandomNum = (min, max) => {
     return Math.floor(Math.random() * (max - min)) + min; // You can remove the Math.floor if you don't want it to be an integer
@@ -28,11 +28,60 @@ Arraycutout = (array, place = -1, value = "unknown") => {
     }
 };
 
-DetectWinToCancel = (MovesS) => {
-    
+DetectWinToCancel = (MovesS, AIteam) => {
+    var ReturnValue = "0";
+    var AllFields = [];
+    var MovesArr = [];
+    var AvailableFields = [];
+
+    var TheMoves = MovesS.split(";")
+
+    for (var x = 0; x <= 8; x++) {
+        MovesArr.push(TheMoves[x])
+        AllFields.push(TheMoves[x].substr(0, 2))
+        if (TheMoves[x].substr(2, 1) == "/") {
+            AvailableFields.push(TheMoves[x].substr(0, 2));
+        }
+    }
+    var Rules = JSON.parse(fs.readFileSync(RulesPath, "utf-8"));
+    var objectKeysArray = Object.keys(Rules)
+    for (var x = 0; x <= 1; x++) {
+        var TeamToCheck = "";
+        if (x == 0) {
+            TeamToCheck = "X";
+        } else {
+            TeamToCheck = "O";
+        }
+        objectKeysArray.forEach(function (objKey) {
+            var OnThreeWin = 0;
+            var objValue = Rules[objKey]
+            var FieldsToWin = objValue;
+            objValue.forEach(WinFields => {
+                MovesArr.forEach(Field => {
+
+                    if (Field == (WinFields + TeamToCheck)) {
+                        FieldsToWin = Arraycutout(FieldsToWin, -1, WinFields)
+                        OnThreeWin++;
+                    }
+                })
+            });
+            if (OnThreeWin == 2) {
+                if (AvailableFields.includes(FieldsToWin[0]) && TeamToCheck != AIteam) {
+                    //console.log("Player is near the win with field: " + FieldsToWin[0])
+                    ReturnValue = FieldsToWin[0];
+                }
+            }
+
+        })
+    }
+    return ReturnValue;
 }
 
 module.exports.PickField = async (MovesS, AIteam, Movecounter) => {
+    if (DetectWinToCancel(MovesS, AIteam) != "0") {
+        //This is OP mode to INSTANTLY block all moves from enemy.
+        return DetectWinToCancel(MovesS, AIteam)
+    }
     var AvilableMoves = [];
     var IfEverythingIsLost = [];
     var TheMoves = MovesS.split(";");
@@ -43,22 +92,18 @@ module.exports.PickField = async (MovesS, AIteam, Movecounter) => {
         }
     }
     IfEverythingIsLost = AvilableMoves;
-    //console.log("Available moves:");
-    //console.table(AvilableMoves);
 
     var GoodMovesToMake = [];
     var MovesDontWork = [];
 
     var OldGames = JSON.parse(fs.readFileSync(OldGamesPath, "utf-8"));
     OldGames.forEach(Agame => {
-        //console.table(Agame)
         if (Agame.winner == AIteam) {
             var Currentpattern = (Movecounter == 0) ? Agame.move0 : (Movecounter == 1) ? Agame.move1 : (Movecounter == 2) ? Agame.move2 : (Movecounter == 3) ? Agame.move3 : (Movecounter == 4) ? Agame.move4 : (Movecounter == 5) ? Agame.move5 : (Movecounter == 6) ? Agame.move6 : (Movecounter == 7) ? Agame.move7 : (Movecounter == 8) ? Agame.move8 : (Movecounter == 9) ? Agame.move9 : "BROKE";
             if (Currentpattern == "BROKE") {
                 console.log("ERROR: Critical game error detected!")
                 exit(0);
             }
-
             if (Currentpattern == MovesS) {
                 console.log("Pattern found!")
                 GoodMovesToMake.push(Agame);
